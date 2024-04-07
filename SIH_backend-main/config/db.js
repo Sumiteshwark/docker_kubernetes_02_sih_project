f43c0962f9ff9b31af3d32d6016a17723e7eba0d
rebooters <sumiteshwarsuman@gmail.com>
2024-04-07 10:42:49 +0530

    mongodb url env updated

--- SIH_backend-main/config/db.js
+++ SIH_backend-main/config/db.js
@@ -3,7 +3,9 @@ import mongoose from "mongoose";
 const connectDB = async () => {
     try{
         // const conn = await mongoose.connect(process.env.MONGO_URL);
-        const conn = await mongoose.connect("mongodb://mongodb:27017/sih");
+        // const conn = await mongoose.connect("mongodb://mongodb:27017/sih");
+        // const conn = await mongoose.connect("mongodb://rebooters:sk1234@mongodb:27017/sih?authSource=admin");
+        const conn = await mongoose.connect(`mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@mongodb:27017/sih?authSource=admin`);
         console.log(`Connected to Mongodb Database ${conn.connection.host}`);
     }
     catch(err){
